5b18cf7b936e320aa0df9554577d3fdf05ba8346
Guilherme Lemos <guilhermelemos@MacBook-Pro-de-Guilherme.local>
2023-05-20 18:21:29 -0400

    edita header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 // import logo from "../assets/images/astronout.png"
 // import logo1 from "../assets/images/astronout1.png"
 import logo1 from '../assets/images/Captura de Tela 2022-10-13 às 19.02.33.jpg';
-import bg from "../assets/images/bg.jpg"
+import bg from "../assets/images/maxresdefault.jpeg"
 import Navbar from './Navbar'
 import "../styles.css"
 
@@ -15,16 +15,13 @@ function Header() {
     <div className="wrapper flex justify-between items-center h-screen w-full px-20 lg:justify-center lg:px-6">
       <div className="content lg:text-center">
         <h1 className='text-white text-5xl font-bold'>Hi! I'm Guilherme</h1>
-        <p className='text-white py-4 max-w-lg '>Lorem ipsum dolor sit amet consectetur  adipisicing elit. In qui impedit  sunt, est molestiae ullam. Saepe voluptatum ducimus adipisci pariatur. adipisicing elit.  In qui impedit ipsum dolor sit amet consectetur</p>
+        <p className='text-white py-4 max-w-lg '>Seja bem-vindo ao meu portifófio! Aqui você encontrará todos os meus projetos desenvolvidos, além das linguagens de programação utilizadas. ENJOY!</p>
       </div>
-      <div className="image banner-astronout lg:hidden absolute w-[300px] flex ">
-      {/* <img className='w-96 ast-img' src={logo1} alt="" /> */}
+      <div className="image lg:hidden w-[300px] flex ">
+        <div className="card-zoom">
+           <img className="card-zoom-image" src={logo1} alt="" />
+         </div>
       </div>
-      <div className="image lg:hidden w-[300px] flex">
-     
-      <img className='w-96 ast-img' src={logo1} alt="" />
-      </div>
-   
     </div>
    </div>
    </>
